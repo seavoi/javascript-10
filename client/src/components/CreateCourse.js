9ae5f9283ce79f7d0050226c74d0f6db269cffa2
Sean V <s.voivoi@gmail.com>
2019-12-07 12:51:36 -0600

    Create Course Submit

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 
 export default class CreateCourse extends Component {
 
+	/* Submit Button */
+	btnSubmit = (event) => {
+		// Prevent Default Actions
+		event.preventDefault();
+
+		// Get Values
+		const title = document.getElementById('title').value;
+		const description = document.getElementById('description').value;
+		const estimatedTime = document.getElementById('estimatedTime').value;
+		const materialsNeeded = document.getElementById('materialsNeeded').value;
+
+		fetch('http://localhost:5000/api/courses', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ title, description, estimatedTime, materialsNeeded })
+		})
+		//.then(checkStatus)
+		.then(res => res.json())
+		.then(data => console.log(data))
+
+	}
+
 	/* Cancel Button */
   btnCancel = (event) => {
+  	// Prevent Default Actions
   	event.preventDefault();
+  	// Send to Index
     this.props.history.push(`/`);
   }
 
@@ -28,7 +54,7 @@ export default class CreateCourse extends Component {
               <div className="course--header">
                 <h4 className="course--label">Course</h4>
                 <div>
-                	<input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..." value="" />
+                	<input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..." />
                 </div>
                 <p>By Joe Smith</p>
               </div>
@@ -44,7 +70,7 @@ export default class CreateCourse extends Component {
                   <li className="course--stats--list--item">
                     <h4>Estimated Time</h4>
                     <div>
-                    	<input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" value="" />
+                    	<input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" />
                   	</div>
                   </li>
                   <li className="course--stats--list--item">
@@ -57,7 +83,7 @@ export default class CreateCourse extends Component {
               </div>
             </div>
             <div className="grid-100 pad-bottom">
-            	<button className="button" type="submit">Create Course</button>
+            	<button className="button" type="submit" onClick={(event) => this.btnSubmit(event)}>Create Course</button>
             	<button className="button button-secondary" onClick={(event) => this.btnCancel(event)}>Cancel</button>
             </div>
           </form>
